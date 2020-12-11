e2a1d98e0cff22df738714b53b38043d78d94dbd
Nils Siegle <nils.siegle@rennschmiede-pforzheim.de>
2020-12-11 16:01:39 +0100

    Included paperbase template

--- src/App.js
+++ src/App.js
@@ -1,23 +1,17 @@
+import * as React from 'react';
+// import ReactDOM from 'react-dom';
 import logo from './logo.svg';
 import './App.css';
+import './Template/Paperbase.js';
+import Paperbase from './Template/Paperbase.js';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <meta name="viewport" content="initial-scale=1, width=device-width" />
+      <Paperbase>
+        Testtext
+      </Paperbase>
     </div>
   );
 }
