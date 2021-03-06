8782009c44fdd51a3ba5f463b6187dd267c14938
Nils Höll <nils.hoell@posteo.de>
2021-03-06 18:17:57 +0100

    D3 test drive

--- src/Dashboard/Dashboard.jsx
+++ src/Dashboard/Dashboard.jsx
@@ -1,6 +1,29 @@
 import React, { PureComponent } from 'react';
 import PropTypes from 'prop-types';
+import rd3 from 'react-d3-library';
+import node from './d3test';
+const RD3Component = rd3.Component;
 //import { Test } from './Dashboard.styles';
+
+class ReactD3Test extends React.Component {
+
+  constructor(props) {
+    super(props);
+    this.state = {d3: ''}
+  }
+
+  componentDidMount() {
+    this.setState({d3: node});
+  }
+
+  render() {
+    return (
+      <div>
+        <RD3Component data={this.state.d3} />
+      </div>
+    )
+  }
+};
 
 class Dashboard extends PureComponent { 
   constructor(props) {
@@ -29,7 +52,8 @@ class Dashboard extends PureComponent {
     }
     return (
       <div className="DashboardWrapper">
-        Dashboard
+        Dashboard:
+        <ReactD3Test />
       </div>
     );
   }
