26673099d1f17ae283870f4f8d01ce6235f378e0
Nils Siegle <nils.siegle@rennschmiede-pforzheim.de>
2021-03-25 10:40:32 +0100

    Added new base files

--- src/App.js
+++ src/App.js
@@ -1,18 +1,32 @@
-import * as React from 'react';
-// import ReactDOM from 'react-dom';
-import logo from './logo.svg';
-import './App.css';
-import './Template/Paperbase.js';
-import Paperbase from './Template/Paperbase.js';
+import express from "express";
+import compression from "compression";  // compresses requests
+import bodyParser from "body-parser";
+import path from "path";
 
-function App() {
-  return (
-    <div className="App">
-      <meta name="viewport" content="initial-scale=1, width=device-width" />
-      <Paperbase>
-      </Paperbase>
-    </div>
-  );
-}
+// Controllers (route handlers)
+import * as homeController from "./controllers/home";
 
-export default App;
+// Create Express server
+const app = express();
+
+// Express configuration
+app.set("port", process.env.PORT || 3000);
+app.set("views", path.join(__dirname, "../views"));
+app.set("view engine", "pug");
+app.use(compression());
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use((req, res, next) => {
+    next();
+});
+
+app.use(
+    express.static(path.join(__dirname, "public"), { maxAge: 31557600000 })
+);
+
+/**
+ * Primary app routes.
+ */
+app.get("/", homeController.index);
+
+export default app;
--- src/Chart/baseChart.jsx
+++ src/Chart/baseChart.jsx
@@ -1,41 +1,8 @@
-import React, {
-    Component
-} from 'react';
 import * as d3 from "d3";
 
-class BaseChart extends Component {
+class BaseChart {
 
     svg;
-
-    constructor(props) {
-        super(props);
-        this.ref = React.createRef();
-    }
-
-    componentDidMount() {
-        this.prepareChart();
-        this.drawChart();
-
-        const self = this;
-        window.addEventListener("resize", () => this.resize(self));
-    }
-
-    componentWillUnmount = () => {
-        window.removeEventListener("resize", this.resize);
-        this.svg.remove();
-    }
-
-    resize(self) {
-        console.log("Resize Detected");
-        self.drawChart();
-        self.forceUpdate();
-    }
-
-    shouldComponentUpdate() {
-        const res = super.shouldComponentUpdate();
-        console.log("Should update: " + res);
-        return res;
-    }
 
     prepareChart() {
         this.svg = d3.select('#chartWrapper')
@@ -51,7 +18,7 @@ class BaseChart extends Component {
 
     render() {
         console.log("Render");
-        return <div id="chartWrapper" ref={this.ref} style={{height: this.props.height ?? "100%", width: this.props.width ?? "100%"}}></div>;
+        return null;
       }
 
 }
