108c793fc54948ea34c37db7dc8ba01ba49fdfac
Nils Höll <33981934+nilshoell@users.noreply.github.com>
2021-04-02 12:26:11 +0200

    Added touch labels to bar chart

--- src/public/js/charts/barChart.ts
+++ src/public/js/charts/barChart.ts
@@ -2,13 +2,17 @@ import BaseChart from "./baseChart.js";
 
 class BarChart extends BaseChart {
 
-    constructor(canvasID:string) {
-        super(canvasID);
+    svg;
+
+    constructor(canvasID:string, baseData = {}) {
+        super(canvasID, baseData);
     }
 
     drawChart(chartData) {
         console.log("Drawing BarChart with data: ");
         console.log(chartData.data);
+
+        let self = this;
 
         let data = chartData.data;
 
@@ -18,7 +22,9 @@ class BarChart extends BaseChart {
         let barWidth = 0.75 * (width / data.length)
         let barSpace = 0.25 * (width / data.length)
 
-        const bars = d3.select('#' + this.canvasID + ' svg')
+        this.svg = d3.select('#' + this.canvasID + ' svg');
+
+        const bars = this.svg
             .selectAll("rect")
             .data(data);
 
@@ -27,9 +33,60 @@ class BarChart extends BaseChart {
             .attr("y", (d:number, i) => 300 - 10 * d)
             .attr("width", barWidth)
             .attr("height", (d:number, i) => d * 10)
-            .attr("fill", "green");
+            .attr("data-value", (d:number) => d)
+            .attr("data-label", "false")
+            .attr("fill", "green")
+            .on("touchstart", function(e, i:number) {
+                self.toggleLabel(e.target);
+            });
 
-        bars.exit().remove();
+        // bars.exit().remove();
+    }
+
+    toggleLabel(target) {
+
+        const attributes = target.attributes
+
+        const parent = target.parentElement
+
+        const val = +attributes['data-value'].value;
+        const x = +attributes['x'].value;
+        const y = +attributes['y'].value;
+        const barWidth = +attributes['width'].value;
+        const barHeight = +attributes['height'].value;
+
+        const labelExists = attributes['data-label'].value;
+
+        if (labelExists == "true") {
+            d3.selectAll('text.numeric_label').remove();
+            d3.selectAll('rect.numeric_label').remove();
+            d3.select(target).attr("data-label", "false");
+            return;
+        }
+
+        d3.select(target).attr("data-label", "true");
+
+        const height = 20;
+        let width = 45;
+
+        if (barWidth > width) {
+            width = barWidth;
+        }
+
+        this.svg.append('rect')
+            .attr("x", x - (width - barWidth) / 2)
+            .attr("y", y - 25)
+            .attr("width", width)
+            .attr("height", height)
+            .attr("rx", 3)
+            .attr("class", "numeric_label")
+            .attr("fill", "#40bf91");
+        this.svg.append('text')
+            .attr("x", x - (width - barWidth) / 2 + width / 2)
+            .attr("y", y - height / 2)
+            .attr("class", "numeric_label")
+            .text(val)
+            .attr("fill", "DimGrey");
     }
 
 }
--- src/public/js/charts/baseChart.ts
+++ src/public/js/charts/baseChart.ts
@@ -1,24 +1,46 @@
 class BaseChart {
 
     svg;
-    canvasID:string;
+    canvasID: string;
+    baseData;
 
-    constructor(canvasID:string) {
+    /**
+     * Initialise a new chart
+     * @param canvasID The ID of the HTML element to draw in
+     * @param baseData Additional parameters to control the base SVG
+     */
+    constructor(canvasID: string, baseData = {}) {
         this.canvasID = canvasID;
+        this.baseData = baseData;
         this.prepareChart();
     }
-    
+
+    /**
+     * Creates a D3 SVG element as specified
+     */
     prepareChart() {
-        console.log("Preparing Chart Canvas on #" + this.canvasID);
+        console.log("Preparing chart canvas on #" + this.canvasID);
         this.svg = d3.select('#' + this.canvasID)
             .append("svg")
-            .attr("width", null ?? "100%")
-            .attr("height", null ?? "100%")
+            .attr("width", this.baseData.width ?? "100%")
+            .attr("height", this.baseData.height ?? "100%")
             .style("margin-left", 10);
     }
 
+    /**
+     * Draw the actual chart (not implemented in the base class)
+     * @param data JS-Object with data, labels and more
+     */
     drawChart(data) {
-        throw "Draw Chart not implemented.";
+        throw "drawChart() not implemented in base class.";
+    }
+
+    /**
+     * Draw axes for the chart
+     * @param data Information for all axes
+     */
+    drawAxes(data) {
+        throw "drawAxes() not implemented in base class.";
     }
 
 }
