dec5ddd719d5bc7613f3858c61bac2084b1ef823
Nils Höll <33981934+nilshoell@users.noreply.github.com>
2021-04-16 16:12:41 +0200

    Started KPI page

--- src/routers/index.ts
+++ src/routers/index.ts
@@ -2,6 +2,7 @@ import Router from "express";
 
 const router = Router();
 
+// List of available pages
 const routes = [
   {id: "home", link:"/", title: "Home", icon: "fa-home"},
   {id: "dashboard", link:"/dashboard", title: "Dashboard", icon: "fa-tachometer-alt"},
@@ -10,20 +11,29 @@ const routes = [
   {id: "personal", link:"/personal", title: "Personal", icon: "fa-user"}
 ]
 
+// Setup standard routes
 routes.forEach(route => {
   router.get(route.link, function(req, res) {
     res.render('pages/' + route.id, {routes, route});
   });
 });
 
-// router.get('/dashboard', function(req, res) {
-//   res.render('pages/dashboard', {
-//   })
+// Additional KPI page
+router.get('/kpi/:id', function(req, res) {
+  const params = req.params;
+  const route = {id: "kpi", link:"/kpi", title: "KPI", icon: "fa-home"};
+  res.render('pages/kpi', {routes, route, params});
+});
+
+router.get('/kpi', function(req, res) {
+  res.status(400);
+  res.render('special/400', {msg: "Please provide a valid KPI ID"});
+});
+
+// router.use(function(req, res) {
+//   res.status(404);
+//   res.render('special/404');
 // });
 
-// router.get('/', function(req, res) {
-//   res.render('pages/home', {
-//   })
-// });
 
 export default router;
