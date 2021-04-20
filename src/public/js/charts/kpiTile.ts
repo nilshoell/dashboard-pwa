08133a2c1d54f192e08e1c9dd9ea6e01b51f13b1
Nils Höll <33981934+nilshoell@users.noreply.github.com>
2021-04-20 14:04:59 +0200

    Implemented improved home screen views

--- src/public/js/charts/kpiTile.ts
+++ src/public/js/charts/kpiTile.ts
@@ -9,7 +9,7 @@ class KPITile extends BaseChart {
     constructor(canvasID:string, baseData = {}) {
         super(canvasID, baseData);
         // Set default margins
-        this.setMargins({bottom: 0, top: 0, right:0, left:0});
+        this.setMargins({bottom: 10, top: (this.baseData.height / 2) + 40, right: 10, left: 10});
     }
 
     drawChart(chartData) {
@@ -17,32 +17,196 @@ class KPITile extends BaseChart {
         this.chartData = chartData;
         const data = chartData.data;
         const margin = this.baseData.margin;
-        console.log("Drawing KPITile with data: ", data);
+        console.log("Drawing KPITile with data: ", chartData.name, data);
+
+        // Setup Scales
+        this.setScales();
 
         this.drawAggregates();
+        this.setBackground();
         this.drawSparkline();
+        this.drawAnnotations();
 
     }
 
     drawAggregates() {
         const data = this.chartData.data.barData
-        // ACT in center
-        // PY top left; deviation below
-        // BUD top right; deviation below
+
+        const d = {
+            act: data[1],
+            py: data[0],
+            py_dev: (data[1] - data[0]) / data[0],
+            bud: data[2],
+            bud_dev: (data[1] - data[2]) / data[2],
+            fc: data[3] ?? 0
+        }
+
+        // Remove existing bg/text on redraw
+        this.svg.selectAll("rect").remove();
+        this.svg.selectAll("text").remove();
+        
+        // Background
         this.svg.append("rect")
             .attr("x", 0)
             .attr("y", 0)
+            .attr("rx", 3)
             .attr("width", this.baseData.width)
             .attr("height", this.baseData.height)
             .attr("fill", "white");
+
+        const textFormat = (value:number|string, format:string = ".2s") => {
+            if (typeof(value) == "number") {
+                return d3.format(format)(value);
+            } else {
+                return value;
+            }
+        }
+
+        const textSmall = (x:number, y:number, value:number, format:string=".2s", anchor:string="start") => {
+            this.svg.append("text")
+                .attr("x", x)
+                .attr("y", y)
+                .text(textFormat(value,format))
+                .attr("fill", "grey")
+                .style("font-size", "large")
+                .style("text-anchor", anchor);
+        }
+
+        const textLarge = (x:number, y:number, value:number|string, format:string=".3s") => {
+            this.svg.append("text")
+                .attr("x", x)
+                .attr("y", y)
+                .text(textFormat(value,format))
+                .attr("fill", "black")
+                .style("font-size", "xx-large")
+                .style("text-anchor", "middle");
+        }
+
+        const textTitle = (x:number, y:number, value:string) => {
+            this.svg.append("text")
+                .attr("x", x)
+                .attr("y", y)
+                .text(value)
+                .attr("fill", "dimgrey")
+                .style("font-size", "x-large")
+                .style("text-anchor", "middle");
+        }
+
+        // Title on Top
+        textTitle(this.baseData.width / 2, 20,this.chartData.name);
+
+        // PY top left; deviation below
+        textSmall(10,45,d.py);
+        textSmall(10,65,d.py_dev,"+.1%");
+
+        // BUD top right; deviation below
+        textSmall(this.baseData.width - 10,45,d.bud,".2s","end");
+        textSmall(this.baseData.width - 10,65,d.bud_dev,"+.1%","end");
+
+        // ACT in center
+        textLarge(this.baseData.width / 2, (this.baseData.height / 2) + 5, d.act);
     }
 
     drawSparkline() {
+
         const data = this.chartData.data.sparkData
-        // Sparkline at bottom
-        // this.setScales();
+
+        this.svg.selectAll("path").remove();
+
+        // Create line generator
+        let line = d3.line()
+            .x((d:any, i:any) => this.xScale(i))
+            .y((d:any) => this.yScale(d))
+            .curve(d3.curveMonotoneX);
+
+        // Setup path object
+        const path = this.svg
+            .selectAll("path")
+            .data(data);
+
+        path.exit().remove();
+
+        // Update data
+        path.join("path")
+            .attr("fill", "none")
+            .attr("stroke", "black")
+            .attr("stroke-width", 1)
+            .attr("d", line(data));
     }
 
+    setBackground() {
+
+        const margin = this.baseData.margin;
+        const backgrounds= d3.selectAll("#" + this.canvasID + " svg rect.sparkline-bg")
+        const backgroundExists = Boolean(backgrounds.size());
+
+        if (backgroundExists) {
+            backgrounds
+                .attr("width", this.baseData.width - margin.y + 4)
+                .attr("height", this.baseData.height - margin.x + 4)
+
+        } else {
+            this.svg.append("rect")
+                .attr("x", margin.left - 2)
+                .attr("y", margin.top - 2)
+                .attr("width", this.baseData.width - margin.x + 4)
+                .attr("height", this.baseData.height - margin.y + 4)
+                .attr("fill", "#b0b0b0")
+                .attr("class", "sparkline-bg");
+        }
+    }
+
+    /**
+     * Draws three additional annotations for min, max and current value
+     */
+    drawAnnotations() {
+        const data = this.chartData.data.sparkData;
+
+        const max = {
+            x: this.xScale(data.findIndex(d => d == d3.max(data))),
+            y: this.yScale(d3.max(data))
+        };
+        const min = {
+            x: this.xScale(data.findIndex(d => d == d3.min(data))),
+            y: this.yScale(d3.min(data))
+        };
+        const current = {
+            x: this.xScale(data.length - 1),
+            y: this.yScale(data[data.length - 1])
+        };
+
+        d3.selectAll("#" + this.canvasID + " svg circle.sparkline-annotation").remove();
+
+        const drawCircle = (coords:any, color:string) => {
+            this.svg.append("circle")
+                .attr("cx", coords.x)
+                .attr("cy", coords.y)
+                .attr("r", 3)
+                .attr("fill", color)
+                .attr("class", "sparkline-annotation");
+        }
+
+        // Draw max marker
+        drawCircle(max, "green");
+
+        // Draw min marker
+        drawCircle(min, "#e10000");
+
+        // Draw current marker
+        drawCircle(current, "steelblue");
+    }
+
+    setScales() {
+        const data = this.chartData.data.sparkData;
+        const margin = this.baseData.margin;
+        this.xScale = d3.scaleLinear()
+            .domain(d3.extent(data, (d:number, i:number) => i))
+            .range([margin.left, this.baseData.width - margin.right]);
+
+        this.yScale = d3.scaleLinear()
+            .domain([d3.min(data, (d:number) => d), d3.max(data, (d:number) => d)]).nice()
+            .range([this.baseData.height - margin.bottom, margin.top]);
+    }
 
 }
 
--- src/public/js/pages/home.ts
+++ src/public/js/pages/home.ts
@@ -10,29 +10,37 @@ $(function () {
 class Home {
 
     charts = [];
+    kpis = [];
 
     constructor() {
 
         const self = this;
 
-        const kpis = [
+        const randomSpark = (min:number, max:number, count:number, end:number) => {
+            let data = [];
+            for (let i = 0; i < count; i++) {
+                data.push(d3.randomInt(min,max)());
+            }
+            data.push(end);
+            return data;
+        }
+
+        this.kpis = [
             {data: {
-                barData: [0],
-                sparkData: [0],
-            }},
+                barData: [5210,4530,6890],
+                sparkData: randomSpark(4000,5000,30,4530),
+            }, name: "Test-KPI 1", rendered: false},
             {data: {
-                barData: [0],
-                sparkData: [0],
-            }},
+                barData: [2550210,7445300,9468990],
+                sparkData: randomSpark(7000000,7500000,30,7445300),
+            }, name: "Test-KPI 2", rendered: false},
             {data: {
-                barData: [0],
-                sparkData: [0],
-            }}
+                barData: [52100,45300,68900],
+                sparkData: randomSpark(40000,45000,30,45300),
+            }, name: "Test-KPI 3", rendered: false}
         ];
 
-        kpis.forEach(function (kpi, index) {
-            self.renderKPI('tile' + (+index + 1), kpi);
-        });
+        self.renderKPI('tile1', this.kpis[0]);
 
         this.configureEventListener();
     }
@@ -40,17 +48,26 @@ class Home {
     configureEventListener() {
         const self = this;
         window.addEventListener('resize', () => self.resizeHandler());
+
+        // Render other charts on first carousel slide
         $('#kpiCarousel').on('slid.bs.carousel', function (e) {
-            self.resizeHandler();
+            if (!self.kpis[e.to].rendered) {
+                self.renderKPI('tile' + (e.to +1), self.kpis[e.to]);
+                self.kpis[e.to].rendered = true;
+            } else {
+                self.charts[e.to].resizeChart();
+            }
         });
     }
 
+    // Render the KPIs
     renderKPI(canvasID:string, chartData) {
         const chart = new KPITile(canvasID);
         chart.drawChart(chartData);
         this.charts.push(chart);
     }
 
+    // Resize all charts
     resizeHandler() {
         this.charts.forEach((chart:KPITile) => {
             chart.resizeChart();
