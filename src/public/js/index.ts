bdc3e848364e7b89bc488ad463842c465a1db16d
Nils Höll <33981934+nilshoell@users.noreply.github.com>
2021-04-27 11:11:49 +0200

    Moved sw back

--- src/public/js/index.ts
+++ src/public/js/index.ts
@@ -53,7 +53,7 @@ class App {
      */
     registerSW() {
         if ("serviceWorker" in navigator) {
-            navigator.serviceWorker.register("/public/service-worker.js", {scope: "/"})
+            navigator.serviceWorker.register("/public/js/service-worker.js", {scope: "/"})
               .then((reg) => {
                 console.log("Service worker registered:", reg);
               }, (err) => {
