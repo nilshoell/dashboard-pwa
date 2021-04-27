c4261d9000eb74b580921442f623861ebf4c6617
Nils Höll <33981934+nilshoell@users.noreply.github.com>
2021-04-27 13:14:56 +0200

    SW insecure mode

--- copyStaticAssets.ts
+++ copyStaticAssets.ts
@@ -6,6 +6,6 @@ shell.mkdir("-p", "dist/public/js/vendor/");
 shell.mkdir("-p", "dist/db/");
 shell.cp("-R", "src/public/images", "dist/public/");
 shell.cp("node_modules/jquery/dist/jquery.min.js", "dist/public/js/vendor/");
-shell.cp("node_modules/bootstrap/dist/js/bootstrap.bundle.min.js", "dist/public/js/vendor/");
+shell.cp("node_modules/bootstrap/dist/js/bootstrap.bundle.min.*", "dist/public/js/vendor/");
 shell.cp("node_modules/d3/dist/d3.min.js", "dist/public/js/vendor/");
 shell.cp("src/public/manifest.json", "dist/public/");
