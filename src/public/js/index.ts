104023a2eb0c82b54be818f20aead17f75ee7f68
Nils Höll <33981934+nilshoell@users.noreply.github.com>
2021-04-27 10:07:49 +0200

    Fixed serviceWorker path

--- src/public/js/index.ts
+++ src/public/js/index.ts
@@ -53,7 +53,7 @@ class App {
      */
     registerSW() {
         if ("serviceWorker" in navigator) {
-            navigator.serviceWorker.register("./service-worker.js")
+            navigator.serviceWorker.register("./public/js/service-worker.js")
               .then((reg) => {
                 console.log("Service worker registered:", reg);
               }, (err) => {
