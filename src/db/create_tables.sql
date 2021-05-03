4843d6d7b4bd1a1b41d55a2a718453ebdb44f358
Nils Höll <33981934+nilshoell@users.noreply.github.com>
2021-05-03 22:16:31 +0200

    Database Updates

--- src/db/create_tables.sql
+++ src/db/create_tables.sql
@@ -1,59 +1,72 @@
-CREATE TABLE IF NOT EXISTS `kpis` (
+-- -----------------------------------------------------
+-- Table `dashboard`.`kpis`
+-- -----------------------------------------------------
+CREATE TABLE IF NOT EXISTS `dashboard`.`kpis` (
   `id` VARCHAR(12) NOT NULL,
   `name` VARCHAR(255) NOT NULL,
-  `shortname` VARCHAR(45) NOT NULL,
+  `shortname` VARCHAR(12) NOT NULL,
   `type` VARCHAR(45) NOT NULL,
-  `parent` VARCHAR(12) NULL DEFAULT NULL,
+  `parent` VARCHAR(12) NOT NULL,
+  `unit` VARCHAR(45) NOT NULL,
+  `direction` VARCHAR(45) NOT NULL,
   `help` LONGTEXT NULL DEFAULT NULL,
-  PRIMARY KEY (`id`));
+  `formula` VARCHAR(255) NULL DEFAULT 'Direct Measurement',
+  PRIMARY KEY (`id`))
+ENGINE = InnoDB;
 
-CREATE TABLE IF NOT EXISTS `measures` (
+
+-- -----------------------------------------------------
+-- Table `dashboard`.`partners`
+-- -----------------------------------------------------
+CREATE TABLE IF NOT EXISTS `dashboard`.`partners` (
+  `id` INT NOT NULL AUTO_INCREMENT,
+  `name` VARCHAR(255) NOT NULL,
+  `shortname` VARCHAR(12) NOT NULL,
+  `region` VARCHAR(45) NOT NULL,
+  PRIMARY KEY (`id`))
+ENGINE = InnoDB;
+
+
+-- -----------------------------------------------------
+-- Table `dashboard`.`products`
+-- -----------------------------------------------------
+CREATE TABLE IF NOT EXISTS `dashboard`.`products` (
+  `id` INT NOT NULL AUTO_INCREMENT,
+  `name` VARCHAR(255) NOT NULL,
+  `shortname` VARCHAR(12) NOT NULL,
+  `parent` INT NOT NULL,
+  PRIMARY KEY (`id`))
+ENGINE = InnoDB;
+
+
+-- -----------------------------------------------------
+-- Table `dashboard`.`measures`
+-- -----------------------------------------------------
+CREATE TABLE IF NOT EXISTS `dashboard`.`measures` (
   `id` INT NOT NULL AUTO_INCREMENT,
   `timestamp` DATETIME NOT NULL DEFAULT NOW(),
   `kpi` VARCHAR(12) NOT NULL,
   `value` DECIMAL(10,2) NOT NULL,
-  `partner` INT DEFAULT NULL,
-  `product` INT DEFAULT NULL,
+  `partner` INT NULL,
+  `product` INT NULL,
+  `scenario` VARCHAR(45) NULL DEFAULT 'AC',
   PRIMARY KEY (`id`),
   INDEX `kpi_id_idx` (`kpi` ASC),
+  INDEX `partner_id_idx` (`partner` ASC),
+  INDEX `product_id_idx` (`product` ASC),
   CONSTRAINT `kpi_id`
     FOREIGN KEY (`kpi`)
-    REFERENCES `kpis` (`id`)
-    ON DELETE NO ACTION ON UPDATE NO ACTION
-  INDEX `partner_id_idx` (`partner` INT),
+    REFERENCES `dashboard`.`kpis` (`id`)
+    ON DELETE NO ACTION
+    ON UPDATE NO ACTION,
   CONSTRAINT `partner_id`
     FOREIGN KEY (`partner`)
-    REFERENCES `partners` (`id`)
-    ON DELETE NO ACTION ON UPDATE NO ACTION
-  INDEX `product_id_idx` (`product` INT),
+    REFERENCES `dashboard`.`partners` (`id`)
+    ON DELETE NO ACTION
+    ON UPDATE NO ACTION,
   CONSTRAINT `product_id`
     FOREIGN KEY (`product`)
-    REFERENCES `products` (`id`)
-    ON DELETE NO ACTION ON UPDATE NO ACTION);
-
-CREATE TABLE IF NOT EXISTS `targets` (
-  `id` INT NOT NULL AUTO_INCREMENT,
-  `kpi` VARCHAR(12) NOT NULL,
-  `type` VARCHAR(45) NOT NULL,
-  `value` DECIMAL(10,2) NOT NULL,
-  PRIMARY KEY (`id`),
-  INDEX `kpi_id_idx` (`kpi` ASC),
-  CONSTRAINT `kpi_id`
-    FOREIGN KEY (`kpi`)
-    REFERENCES `kpis` (`id`)
+    REFERENCES `dashboard`.`products` (`id`)
     ON DELETE NO ACTION
-    ON UPDATE NO ACTION);
-
-CREATE TABLE IF NOT EXISTS `partners` (
-  `id` INT NOT NULL AUTO_INCREMENT,
-  `name` VARCHAR(255) NOT NULL,
-  `shortname` VARCHAR(45) NOT NULL,
-  `region` VARCHAR(45) NOT NULL
-  PRIMARY KEY (`id`));
-
-CREATE TABLE IF NOT EXISTS `products` (
-  `id` INT NOT NULL AUTO_INCREMENT,
-  `name` VARCHAR(255) NOT NULL,
-  `shortname` VARCHAR(45) NOT NULL,
-  `parent` VARCHAR(45) NOT NULL
-  PRIMARY KEY (`id`));
+    ON UPDATE NO ACTION)
+ENGINE = InnoDB;
