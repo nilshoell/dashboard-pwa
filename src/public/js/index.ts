0959e8d7b8b02f86a99daf4f4d0c0ae6eacebf44
Nils Höll <33981934+nilshoell@users.noreply.github.com>
2021-05-04 12:26:24 +0200

    Fixed eslint errors

--- src/public/js/index.ts
+++ src/public/js/index.ts
@@ -67,24 +67,24 @@ class App {
 
     setupNotifications() {
         Notification.requestPermission(function(status) {
-            console.log('Notification permission status:', status);
+            console.log("Notification permission status:", status);
         });
         
     }
 
     displayNotification() {
-        if (Notification.permission == 'granted') {
+        if (Notification.permission == "granted") {
           navigator.serviceWorker.getRegistration().then(function(reg) {
-            var options = {
-              body: 'Here is a notification body!',
-              icon: 'images/example.png',
+            const options = {
+              body: "Here is a notification body!",
+              icon: "public/images/icon.png",
               vibrate: [100, 50, 100],
               data: {
                 dateOfArrival: Date.now(),
                 primaryKey: 1
               }
             };
-            reg.showNotification('Hello world!', options);
+            reg.showNotification("Hello world!", options);
           });
         }
       }
