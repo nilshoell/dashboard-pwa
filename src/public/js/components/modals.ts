493aff03dea40519ea965edef0665c4f3a866825
Nils Höll <33981934+nilshoell@users.noreply.github.com>
2021-05-14 11:56:00 +0200

    Implemented the dashboard page

--- src/public/js/components/modals.ts
+++ src/public/js/components/modals.ts
@@ -13,7 +13,7 @@ function fill(title:string, body:Record<string, any>[], filter = {}) {
     let bodyContent = "";
 
     if (!Helper.emptyObj(filter)) {
-        bodyContent += "<b>Period: </b>" + filter["period"] + "<b> Scenario: </b>" + filter["scenario"] + "</br><hr>";
+        bodyContent += "<b>Period: </b>" + filter["period"] ?? "AC" + "<b> Scenario: </b>" + filter["scenario"] ?? "YTD" + "</br><hr>";
     }
 
     body.forEach(entry => {
--- src/public/js/pages/dashboard.ts
+++ src/public/js/pages/dashboard.ts
@@ -1,41 +1,96 @@
+import * as API from "./../components/api.js";
+import * as Helper from "./../components/helperFunctions.js";
 import KPIBar from "./../charts/kpiBar.js";
+import Sparkline from "./../charts/sparkline.js";
 
-$(function () {
-    new Dashboard();
+$(async function () {
+    const dashboard = new Dashboard();
+    await dashboard.getMasterData();
+    await dashboard.getChartData();
+    dashboard.kpis.forEach((kpi, i) => {
+        dashboard.renderBar("kpibar_" + (i+1), kpi);
+        dashboard.renderSpark("sparkline_" + (i+1), kpi);
+    });
 });
 
 class Dashboard {
 
     charts = [];
+    kpis = [];
 
     constructor() {
 
-        const self = this;
-
-        const kpis = [
-            {data: [3000,1500,3500]},
-            {data: [1500,6485,9800,10000]},
-            {data: [1234,9487,3500]},
-            {data: [7123,7892,9644]},
-            {data: [4678,1500,4584,4500]},
-            {data: [9728,6758,3884,3500]}
+        this.kpis = [
+            {id: "9efcb5361969", masterdata: {}, filter: {period: "YTD"}},
+            {id: "53c6cd0b443b", masterdata: {}, filter: {period: "YTD"}},
+            {id: "74351e8d7097", masterdata: {}, filter: {period: "YTD"}},
+            {id: "eb9f9dc3efb7", masterdata: {}, filter: {period: "YTD"}},
+            {id: "3f6e4e8df453", masterdata: {}, filter: {period: "YTD"}},
+            {id: "255e926dc950", masterdata: {}, filter: {period: "YTD"}}
         ];
         
-        kpis.forEach(function (kpi, index) {
-            self.renderKPI("kpiBar" + (+index + 1), kpi);
-        });
-
         this.configureEventListener();
     }
 
     configureEventListener() {
         const self = this;
-        // $(document).on("longTouch", ".kpi-bar", () => self.longTouch(this));
         window.addEventListener("resize", () => self.resizeHandler());
+        
+        // Toggle view button
+        $(document).on("click", "#toggleSwitch", () => {
+            const invisible = $(".kpi-col.d-none");
+            const visible = $(".kpi-col").not("d-none");
+
+            visible.addClass("d-none d-sm-block");
+            invisible.removeClass("d-none d-sm-block");
+
+            this.resizeHandler();
+        });
     }
 
-    renderKPI(canvasID:string, chartData) {
+    async getMasterData() {
+        for (let i = 0; i < this.kpis.length; i++) {
+            const id = this.kpis[i].id;
+            this.kpis[i].masterdata = await API.callApi("masterdata", id);
+        }
+    }
+
+    async getChartData() {
+        for (let i = 0; i < this.kpis.length; i++) {
+            const id = this.kpis[i].id;
+            if (this.kpis[i]["masterdata"].aggregate === "sum") {
+                this.kpis[i]["barData"] = await API.getBarData(id);
+                this.kpis[i]["sparkData"] = await API.getCumulativeTimeData(id);
+            } else {
+                this.kpis[i]["barData"] = await API.getLatestBarData(id);
+                const data = await API.getTimeData(id);
+                this.kpis[i]["sparkData"] = await Helper.movingAvg(data, 14);
+            }
+        }
+    }
+
+    renderBar(canvasID:string, kpi) {
         const chart = new KPIBar(canvasID);
+        const chartData = {};
+
+        chartData["kpi"] = kpi.id;
+        chartData["data"] = kpi.barData;
+        chartData["masterdata"] = kpi.masterdata;
+        chartData["filter"] = kpi.filter;
+
+        chart.drawChart(chartData);
+        this.charts.push(chart);
+    }
+
+    renderSpark(canvasID:string, kpi) {
+        const chart = new Sparkline(canvasID);
+        const chartData = {};
+
+        chartData["kpi"] = kpi.id;
+        chartData["data"] = kpi.sparkData;
+        chartData["masterdata"] = kpi.masterdata;
+        chartData["filter"] = kpi.filter;
+
         chart.drawChart(chartData);
         this.charts.push(chart);
     }
